ca256534bcc765c18e9a37d3ee2ec5244a11317f
Edgar Jose Rojas Lopez <edgarjrojasl@gmail.com>
2020-08-02 23:17:36 -0400

    Updates

---
+++ precache-manifest.938c2f9e82b22aa1d300a25098768acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d455c5f52b4d10f7adb2efc7682c722",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3d957631db96930faccb",
+    "url": "/monsters-rolodex/static/css/main.c9996082.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d957631db96930faccb",
+    "url": "/monsters-rolodex/static/js/main.9e8596a7.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
